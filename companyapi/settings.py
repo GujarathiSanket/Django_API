4be3cec295046ecd37da19fcd36079a3d74119a7
sanket gujrathi <sanketgujju2298@gmail.com>
2025-11-10 15:01:41 +0530

    Routes Fixed

--- companyapi/settings.py
+++ companyapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jpvabgo2xsmsttly+!n)jh*84wm5b=m1m^3_a5)ngi@*sm4&s8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
